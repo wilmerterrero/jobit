8ebacdd7e6014014f919bff6ad3410270982c4cb
FranklinNoah <78183281+FranklinNoah@users.noreply.github.com>
2021-01-31 16:03:54 -0400

    Update login.tsx

--- jobit-client/pages/login.tsx
+++ jobit-client/pages/login.tsx
@@ -5,23 +5,22 @@ const Login = () => {
   <div>
       <div class="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col m-5">
           <div class="mb-4">
-              <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
-        Username
+              <label class="block text-white text-sm font-bold mb-2" for="username">
+        Email
         </label>
         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username"/>
         </div>
         <div class="mb-6">
-            <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+            <label class="block text-white text-sm font-bold mb-2" for="password">
         Password
         </label>
         <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************"/>
-        <p class="text-red text-xs italic">Please choose a password.</p>
         </div>
         <div class="flex items-center justify-between">
             <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">
         Sign In
         </button>
-        <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
+        <a class="inline-block align-baseline font-bold text-sm text-white hover:text-blue-darker" href="#">
         Forgot Password?
         </a>
         </div>
