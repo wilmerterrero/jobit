8976f169dc7dc55aa5abaede370a7e85dd14806d
wilmerterrero <wilmerterrero1922@gmail.com>
2021-02-07 20:46:20 -0400

    login and create account validation with yup and formik

--- jobit-client/pages/create-account.tsx
+++ jobit-client/pages/create-account.tsx
@@ -1,74 +1,147 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+interface CreateAccountValues {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const CreateAccount = () => {
+
+  const initialCreateAccountValuesValues: CreateAccountValues = {
+    username: '',
+    email: '',
+    password: ''
+  }
+
+  const formik = useFormik({
+    initialValues: initialCreateAccountValuesValues,
+    validationSchema: Yup.object({
+      username: Yup.string()
+               .required('The username is mandatory'),
+      email: Yup.string()
+                .email('The email is not valid')
+                .required('The email is mandatory'),
+      password: Yup.string()
+                .required('The password is mandatory')
+                .min(6, 'The password must contain 6 characters')
+    }),
+    onSubmit: values => {
+      console.log(values);
+    }
+  });
+
   return (
     <Layout>
       <div
         className="bg-cover bg-center h-screen flex justify-center items-center"
         style={{ backgroundImage: `url("img/pattern.svg")` }}
       >
-        <div className="bg-gray-800 md:w-1/2 shadow-md rounded px-8 pt-6 pb-8 flex flex-col">
+        <form 
+          className="bg-gray-800 md:w-1/2 shadow-md rounded px-8 pt-6 pb-8 flex flex-col"
+          onSubmit={formik.handleSubmit}
+        >
           <h2 className="text-2xl text-yellow-200 font-bold mb-4 underline">
             Create an account
           </h2>
+
           <div className="mb-4">
+
+            { formik.touched.username && formik.errors.username ? (
+                  <div className="my-2 p-2 bg-gray-200 border-l-4 border-red-500 text-red-700">
+                    <p className="font-bold">Error</p>
+                    <p>{ formik.errors.username }</p>
+                  </div>
+                ) : null }
+
             <label
               className="block text-white text-sm font-bold mb-2"
               htmlFor="username"
             >
               Username
             </label>
+
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
               id="username"
               type="text"
               placeholder="Username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+
           </div>
+
           <div className="mb-4">
+
+            { formik.touched.email && formik.errors.email ? (
+                    <div className="my-2 p-2 bg-gray-200 border-l-4 border-red-500 text-red-700">
+                      <p className="font-bold">Error</p>
+                      <p>{ formik.errors.email }</p>
+                    </div>
+                  ) : null }
+
             <label
               className="block text-white text-sm font-bold mb-2"
               htmlFor="email"
             >
               Email
             </label>
+
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
               id="email"
               type="email"
               placeholder="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+
           </div>
+
           <div className="mb-6">
+
+            { formik.touched.password && formik.errors.password ? (
+                    <div className="my-2 p-2 bg-gray-200 border-l-4 border-red-500 text-red-700">
+                      <p className="font-bold">Error</p>
+                      <p>{ formik.errors.password }</p>
+                    </div>
+                  ) : null }
+
             <label
               className="block text-white text-sm font-bold mb-2"
               htmlFor="password"
             >
               Password
             </label>
+
             <input
               className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
               id="password"
               type="password"
               placeholder="Password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+
           </div>
+
           <div className="block md:flex items-center justify-between">
             <button
               className="bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block"
-              type="button"
+              type="submit"
             >
-              Sign In
+              Create Account
             </button>
-            <a
-              className="inline-block align-baseline font-bold text-sm text-white hover:text-blue-darker"
-              href="#"
-            >
-              Forgot Password?
-            </a>
           </div>
-        </div>
+
+        </form>
       </div>
     </Layout>
   );
--- jobit-client/pages/login.tsx
+++ jobit-client/pages/login.tsx
@@ -1,49 +1,106 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+interface LoginValues {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+
+  const initialLoginValues: LoginValues = {
+    email: '',
+    password: ''
+  }
+
+  const formik = useFormik({
+    initialValues: initialLoginValues,
+    validationSchema: Yup.object({
+      email: Yup.string()
+                .email('The email is not valid')
+                .required('The email is mandatory'),
+      password: Yup.string()
+                   .required('The password is mandatory')
+    }),
+    onSubmit: values => {
+      console.log(values);
+    }
+  });
+
   return (
     <Layout>
       <div
         className="bg-cover bg-center h-screen flex justify-center items-center"
         style={{ backgroundImage: `url("img/pattern.svg")` }}
       >
-        <div className="bg-gray-800 md:w-1/2 shadow-md rounded px-8 pt-6 pb-8 flex flex-col">
+        <form 
+          className="bg-gray-800 md:w-1/2 shadow-md rounded px-8 pt-6 pb-8 flex flex-col"
+          onSubmit={formik.handleSubmit}
+        >
           <h2 className="text-2xl text-yellow-200 font-bold mb-4 underline">
             Sign In
           </h2>
           <div className="mb-4">
+
+            { formik.touched.email && formik.errors.email ? (
+                    <div className="my-2 p-2 bg-gray-200 border-l-4 border-red-500 text-red-700">
+                      <p className="font-bold">Error</p>
+                      <p>{ formik.errors.email }</p>
+                    </div>
+                  ) : null }
+
             <label
               className="block text-white text-sm font-bold mb-2"
               htmlFor="email"
             >
               Email
             </label>
+
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
               id="email"
               type="email"
               placeholder="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+
           </div>
           <div className="mb-6">
+
+            { formik.touched.password && formik.errors.password ? (
+                    <div className="my-2 p-2 bg-gray-200 border-l-4 border-red-500 text-red-700">
+                      <p className="font-bold">Error</p>
+                      <p>{ formik.errors.password }</p>
+                    </div>
+                  ) : null }
+
             <label
               className="block text-white text-sm font-bold mb-2"
               htmlFor="password"
             >
               Password
             </label>
+
             <input
               className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
               id="password"
               type="password"
               placeholder="Password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+
           </div>
+
           <div className="block md:flex items-center justify-between">
             <button
               className="bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block"
-              type="button"
+              type="submit"
             >
               Sign In
             </button>
@@ -54,7 +111,8 @@ const Login = () => {
               Forgot Password?
             </a>
           </div>
-        </div>
+
+        </form>
       </div>
     </Layout>
   );
