7b5bf295365bc4f2ad329d0817d08def2ba17e1d
Moises <MoisesTabar.013@gmail.com>
2021-03-02 01:45:33 -0400

    Preparing deploy adding database v2

--- jobit-server/ormconfig.js
+++ jobit-server/ormconfig.js
@@ -2,6 +2,6 @@ module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "synchronize": true,
-    "entities": ["dist/src/models/*.js"],
+    "entities": ["./dist/src/models/*.js"],
     "migrations": ["./dist/src/exports/*.js"]
 }
