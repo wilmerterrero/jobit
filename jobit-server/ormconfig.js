63cd5d5d66b19746ea52f1d65e6605dc16b9b966
Moises <MoisesTabar.013@gmail.com>
2021-03-02 02:03:18 -0400

    Preparing migrations and cloud db

--- jobit-server/ormconfig.js
+++ jobit-server/ormconfig.js
@@ -2,6 +2,12 @@ module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "synchronize": true,
-    "entities": ["./dist/src/models/*.js"],
-    "migrations": ["./dist/src/exports/*.js"]
+    "entities": ["dist/src/models/**/*.js"],
+    "migrations": ["dist/src/exports/**/*.js"],
+    "cli": { 
+        "migrationsDir": [
+            "src/exports"
+        ]
+    },
+    "entitiesDir": "src/models"
 }
